ad071dfc9eeb676e8c9adb45bab55948e1cfed02
JiangZeminFans <73158238+JiangZeminFans@users.noreply.github.com>
2022-12-24 01:10:19 +0800

    Delete CS232/homeworks/compiler/src/ast/decl directory

--- CS232/homeworks/compiler/src/ast/decl/DimensionAST.cpp
+++
@@ -1,9 +0,0 @@
-//
-// Created by 86180 on 2022/12/22.
-//
-
-#include "DimensionAST.h"
-
-void DimensionAST::add(LowAST *exp) {
-    this->exps.push_back(unique_ptr<LowAST>(exp));
-}
--- CS232/homeworks/compiler/src/ast/decl/IdDefList.cpp
+++
@@ -1,9 +0,0 @@
-//
-// Created by 86180 on 2022/12/22.
-//
-
-#include "IdDefList.h"
-
-void IdDefList::add(IdDefAST *idDefAst) {
-    this->defList.push_back(unique_ptr<IdDefAST>(idDefAst));
-}
