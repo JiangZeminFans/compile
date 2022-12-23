65600afd32d0a07046d05219c4f08d53de1648de
LazySheeeeep <121164210+LazySheeeeep@users.noreply.github.com>
2022-12-24 01:05:41 +0800

    Combination of all asts

--- CS232/homeworks/compiler/src/ast/AST.cpp
+++ CS232/homeworks/compiler/src/ast/AST.cpp
@@ -7,3 +7,244 @@
 void AST::accept(Visitor &visitor) {
     visitor.visit(this);
 }
+
+void AndAST::setExp(AndAST* low, EqExpAST* high) {
+    this->low = unique_ptr<AndAST>(low);
+    this->high = unique_ptr<EqExpAST>(high);
+}
+
+void BlockAST::setSentences(Sentences* sentences) {
+    this->sentences.swap(sentences->sentences);
+}
+
+void CallAST::setId(string* id) {
+    this->id = unique_ptr<string>(id);
+}
+
+void CompUnitAST::add(DecDefAST* decDef) {
+    this->decDefList.push_back(unique_ptr<DecDefAST>(decDef));
+}
+
+void DecAST::setIdDefList(IdDefList* defList) {
+    this->defList.swap(defList->defList);
+}
+
+void DecDefAST::setDecl(DecAST* decl) {
+    this->Decl = unique_ptr<DecAST>(decl);
+}
+
+void DecDefAST::setDef(FuncDefAST* def) {
+    this->funcDef = unique_ptr<FuncDefAST>(def);
+}
+
+void DimensionAST::add(LowAST* exp) {
+    this->exps.push_back(unique_ptr<LowAST>(exp));
+}
+
+void EqExpAST::setExp(EqExpAST* low, EQ_OP op, RelExpAST* high) {
+    this->low = unique_ptr<EqExpAST>(low);
+    this->high = unique_ptr<RelExpAST>(high);
+    this->op = op;
+}
+
+void FuncDefAST::setFuncParamList(FuncParamList funcParamList) {
+    this->funcParamList.swap(funcParamList.list);
+}
+
+void FuncDefAST::setFuncType(TYPE funcType) {
+    FuncDefAST::funcType = funcType;
+}
+
+void FuncDefAST::setBlock(BlockAST *block) {
+    this->block=unique_ptr<BlockAST>(block);
+}
+
+void FuncDefAST::setId(string *id) {
+    this->id=unique_ptr<string>(id);
+}
+
+void FuncParamList::add(FuncParamAST* funcParamAst) {
+    this->list.push_back(unique_ptr<FuncParamAST>(funcParamAst));
+}
+
+void FuncRParamList::add(LowAST* realParams) {
+    this->realParams.push_back(unique_ptr<LowAST>(realParams));
+}
+
+void HighAST::setOp(HIGH_OP op) {
+    HighAST::op = op;
+}
+
+void HighAST::setModExp(HighAST* factor) {
+    setOp(MOD_OP);
+    this->high = unique_ptr<HighAST>(factor);
+}
+
+void HighAST::setMulExp(HighAST* factor) {
+    setOp(MUL_OP);
+    this->high = unique_ptr<HighAST>(factor);
+}
+
+void HighAST::setDivExp(HighAST* factor) {
+    setOp(DIV_OP);
+    this->high = unique_ptr<HighAST>(factor);
+}
+
+void HighAST::setUnaryExp(UnaryAST* unary) {
+    this->unary = unique_ptr<UnaryAST>(unary);
+}
+
+void IdDefAST::setId(string* id) {
+    this->id = unique_ptr<string>(id);
+}
+
+void IdDefAST::setInitVal(InitValAST* initVal) {
+    this->initVal = unique_ptr<InitValAST>(initVal);
+}
+
+void IdDefAST::setDimensions(DimensionAST* dimensions) {
+    this->dimensions.swap(dimensions->exps);
+}
+
+void IdDefList::add(IdDefAST* idDefAst) {
+    this->defList.push_back(unique_ptr<IdDefAST>(idDefAst));
+}
+
+void InitValAST::setExpression(LowAST* expression) {
+    this->exp = unique_ptr<LowAST>(expression);
+}
+
+void InitValAST::setInitValList(InitValList* list) {
+    this->initValList.swap(list->initValList);
+}
+
+void IterationStmtAST::setStmt(StmtAST* stmt) {
+    this->stmt = unique_ptr<StmtAST>(stmt);
+}
+
+void IterationStmtAST::setCondition(OrAST* cond) {
+    this->cond = unique_ptr<OrAST>(cond);
+}
+
+void InitValList::add(InitValAST* initVal) {
+    this->initValList.push_back(unique_ptr<InitValAST>(initVal));
+}
+
+void LeftValAST::setId(string* id) {
+    this->id = unique_ptr<string>(id);
+}
+
+void LeftValAST::setDimensions(DimensionAST* dimension) {
+    this->dimensions.swap(dimension->exps);
+}
+
+void LowAST::setExpr(LowAST* low, LOW_OP op, HighAST* high) {
+    this->low = unique_ptr<LowAST>(low);
+    this->op = op;
+    this->high = unique_ptr<HighAST>(high);
+}
+
+void LowAST::setHighExp(HighAST* high) {
+    this->high = unique_ptr<HighAST>(high);
+}
+
+void OrAST::setExp(OrAST* low, AndAST* high) {
+    this->low = unique_ptr<OrAST>(low);
+    this->high = unique_ptr<AndAST>(high);
+}
+
+void PrimaryExpAST::setLeftVal(LeftValAST* leftVal) {
+    this->leftVal = unique_ptr<LeftValAST>(leftVal);
+}
+
+void PrimaryExpAST::setExpression(LowAST* exp) {
+    this->exp = unique_ptr<LowAST>(exp);
+}
+
+void PrimaryExpAST::setNumber(NumberAST* num) {
+    this->number = unique_ptr<NumberAST>(num);
+}
+
+void RelExpAST::setExp(RelExpAST* low, REL_OP op, LowAST* high) {
+    this->low = unique_ptr<RelExpAST>(low);
+    this->op = op;
+    this->high = unique_ptr<LowAST>(high);
+}
+
+void ReturnStmtAST::setExpression(LowAST* expression) {
+    this->exp = unique_ptr<LowAST>(expression);
+}
+
+void SelectStmtAST::setIf(StmtAST* ifStmt) {
+    this->ifStmt = unique_ptr<StmtAST>(ifStmt);
+}
+
+void SelectStmtAST::setElse(StmtAST* elseStmt) {
+    this->elseStmt = unique_ptr<StmtAST>(elseStmt);
+}
+
+void SelectStmtAST::setCondition(OrAST* cond) {
+    this->cond = unique_ptr<OrAST>(cond);
+}
+
+void SentenceAST::setDecl(DecAST* decl) {
+    this->decl = unique_ptr<DecAST>(decl);
+}
+
+void SentenceAST::setStmt(StmtAST* stmt) {
+    this->stmt = unique_ptr<StmtAST>(stmt);
+}
+
+void Sentences::add(SentenceAST* sentence) {
+    sentences.push_back(unique_ptr<SentenceAST>(sentence));
+}
+
+void StmtAST::setStmtType(STMT_TYPE stmtType) {
+    StmtAST::stmtType = stmtType;
+}
+
+void StmtAST::setLeftVal(LeftValAST* leftVal) {
+    setStmtType(ASS_T);
+    this->leftVal = unique_ptr<LeftValAST>(leftVal);
+}
+
+void StmtAST::setExpression(LowAST* expression) {
+    setStmtType(EXP_T);
+    this->exp = unique_ptr<LowAST>(expression);
+}
+
+void StmtAST::setBlock(BlockAST* block) {
+    setStmtType(BLK_T);
+    this->block = unique_ptr<BlockAST>(block);
+}
+
+void StmtAST::setReturn(ReturnStmtAST* returnStmt) {
+    setStmtType(RET_T);
+    this->ret = unique_ptr<ReturnStmtAST>(returnStmt);
+}
+
+void StmtAST::setSelect(SelectStmtAST* select) {
+    setStmtType(SEL_T);
+    this->sel = unique_ptr<SelectStmtAST>(select);
+}
+
+void StmtAST::setIteration(IterationStmtAST* iteration) {
+    setStmtType(ITR_T);
+    this->itr = unique_ptr<IterationStmtAST>(iteration);
+}
+
+void UnaryAST::setPrimaryExp(PrimaryExpAST* primaryExp) {
+    UnaryAST::primaryExp = unique_ptr<PrimaryExpAST>(primaryExp);
+}
+
+void UnaryAST::setOp(U_OP op) {
+    UnaryAST::op = op;
+}
+
+void UnaryAST::setCall(CallAST* call) {
+    this->call = unique_ptr<CallAST>(call);
+}
+
+void UnaryAST::setUnary(UnaryAST* unaryAst) {
+    this->unary = unique_ptr<UnaryAST>(unaryAst);
+}
--- CS232/homeworks/compiler/src/ast/AST.h
+++ CS232/homeworks/compiler/src/ast/AST.h
@@ -7,8 +7,7 @@
 
 #include <iostream>
 #include <vector>
-#include "memory"
-#include "Visitor.h"
+#include <memory>
 #include "../base.h"
 using namespace std;
 
@@ -18,6 +17,305 @@ public:
     AST()=default;
     virtual ~AST()=default;
 
+};
+
+class AndAST : public AST {
+public:
+    unique_ptr<AndAST> low;
+    unique_ptr<EqExpAST> high;
+
+    void setExp(AndAST* low, EqExpAST* high);
+};
+
+class BlockAST : public AST {
+public:
+    vector<unique_ptr<SentenceAST>> sentences;
+    void setSentences(Sentences* sentences);
+};
+
+//函数引用
+class CallAST : public AST {
+public:
+    unique_ptr<string> id;
+    vector<unique_ptr<LowAST>> funcParamList;
+
+    void setId(string* id);
+    // void setFuncParams();
+};
+
+class CompUnitAST : public AST {
+public:
+    vector<unique_ptr<DecDefAST>> decDefList;
+    void add(DecDefAST* decDef);
+};
+
+class DecAST : public AST {
+public:
+    TYPE bType;
+    bool isConst;
+    vector<unique_ptr<IdDefAST>> defList;//声明表
+    void setIdDefList(IdDefList* defList);
+};
+
+class DecDefAST : public AST {
+public:
+    unique_ptr<DecAST> Decl;
+    unique_ptr<FuncDefAST> funcDef;
+
+    void setDecl(DecAST* decl);
+    void setDef(FuncDefAST* def);
+};
+
+class DimensionAST : public AST {
+public:
+    vector<unique_ptr<LowAST>> exps;
+
+    void add(LowAST* exp);
+};
+
+class EqExpAST : public AST {
+public:
+    unique_ptr<RelExpAST> high;
+    unique_ptr<EqExpAST> low;
+    EQ_OP op;
+    void setExp(EqExpAST* low, EQ_OP op, RelExpAST* high);
+};
+
+class FuncParamAST : public AST {
+public:
+    TYPE bType;
+    unique_ptr<string> id;
+    bool isArray = false;
+    vector<unique_ptr<LowAST>> arrays;
+
+};
+
+class FuncDefAST : public AST {
+public:
+    TYPE funcType;
+    unique_ptr<string> id;
+    vector<unique_ptr<FuncParamAST>> funcParamList;
+    unique_ptr<BlockAST> block = nullptr;
+
+    void setId(string* id);
+
+    void setFuncParamList(FuncParamList funcParamList);
+
+    void setFuncType(TYPE funcType);
+
+    void setBlock(BlockAST* block);
+
+};
+
+class FuncParamAST : public AST {
+public:
+    TYPE bType;
+    unique_ptr<string> id;
+    bool isArray = false;
+    vector<unique_ptr<LowAST>> arrays;
+
+};
+
+class FuncParamList : public AST {
+public:
+    vector<unique_ptr<FuncParamAST>> list;
+    void add(FuncParamAST* funcParamAst);
+};
+
+class FuncRParamList : public AST {
+public:
+    vector<unique_ptr<LowAST>> realParams;
+    void add(LowAST* realParams);
+};
+
+//*,/,%
+class HighAST :public AST {
+public:
+    HIGH_OP op;
+    unique_ptr<UnaryAST> unary;
+    unique_ptr<HighAST> high;
+
+    void setOp(HIGH_OP op);
+    void setModExp(HighAST* factor);
+    void setMulExp(HighAST* factor);
+    void setDivExp(HighAST* factor);
+    void setUnaryExp(UnaryAST* unary);
+
+};
+
+class IdDefAST : public AST {
+public:
+    unique_ptr<string> id;
+    unique_ptr<InitValAST> initVal;
+    vector<unique_ptr<LowAST>> dimensions;
+
+    void setId(string* id);
+
+    void setInitVal(InitValAST* initVal);
+
+    void setDimensions(DimensionAST* dimensions);
+
+};
+
+class IdDefList {
+public:
+    vector<unique_ptr<IdDefAST>> defList;
+    IdDefList() = default;
+    void add(IdDefAST* idDefAst);
+};
+
+class InitValAST : public AST {
+public:
+    unique_ptr<LowAST> exp;
+    vector<unique_ptr<InitValAST>> initValList;
+    void setExpression(LowAST* exp);
+    void setInitValList(InitValList* list);
+
+};
+
+class IterationStmtAST : public AST {
+public:
+    unique_ptr<StmtAST> stmt;
+    unique_ptr<OrAST> cond;
+
+    void setStmt(StmtAST* stmt);
+    void setCondition(OrAST* condition);
+};
+
+class InitValList {
+public:
+    vector<unique_ptr<InitValAST>> initValList;
+    InitValList() = default;
+    void add(InitValAST* initVal);
+};
+
+class LeftValAST : public AST {
+public:
+    unique_ptr<string> id;
+    vector<unique_ptr<LowAST>> dimensions;
+    void setId(string* id);
+    void setDimensions(DimensionAST* dimensions);
+};
+
+class NumberAST : public AST {
+public:
+    bool isInt;
+    union {
+        int int_t;
+        float float_t;
+    };
+};
+
+//+,-
+class LowAST : public AST {
+public:
+    unique_ptr<LowAST> low;
+    unique_ptr<HighAST> high;
+    LOW_OP op;
+    void setExpr(LowAST* low, LOW_OP op, HighAST* high);
+    void setHighExp(HighAST* high);
+};
+
+class OrAST : public AST {
+public:
+    unique_ptr<OrAST> low;
+    unique_ptr<AndAST> high;
+
+    void setExp(OrAST* low, AndAST* high);
+};
+
+class PrimaryExpAST : public AST {
+public:
+    unique_ptr<LeftValAST> leftVal;
+    unique_ptr<LowAST> exp;
+    unique_ptr<NumberAST> number;
+    void setLeftVal(LeftValAST*);
+    void setExpression(LowAST*);
+    void setNumber(NumberAST*);
+};
+
+class RelExpAST : public AST {
+public:
+    unique_ptr<RelExpAST> low;
+    unique_ptr<LowAST> high;
+    REL_OP op;
+    void setExp(RelExpAST* low, REL_OP op, LowAST* high);
+};
+
+class ReturnStmtAST : public AST {
+public:
+    unique_ptr<LowAST> exp;
+    void setExpression(LowAST* expression);
+};
+
+class SelectStmtAST : public AST {
+public:
+    unique_ptr<StmtAST> ifStmt;
+    unique_ptr<StmtAST> elseStmt;
+
+    unique_ptr<OrStmt> cond;
+
+    void setIf(StmtAST* ifStmt);
+    void setElse(StmtAST* elseStmt);
+    void setCondition(OrAST* cond);
+};
+
+class SentenceAST : public AST {
+public:
+    unique_ptr<DecAST> decl;
+    unique_ptr<StmtAST> stmt;
+    void setDecl(DecAST* decl);
+    void setStmt(StmtAST* stmt);
+};
+
+class Sentences : public AST {
+public:
+    vector<unique_ptr<SentenceAST>> sentences;
+    void add(SentenceAST* sentence);
+};
+
+class StmtAST : public AST {
+public:
+    STMT_TYPE  stmtType;
+
+    unique_ptr<ReturnStmtAST> ret = nullptr;
+    unique_ptr<SelectStmtAST> sel = nullptr;
+    unique_ptr<IterationStmtAST> itr = nullptr;
+
+    unique_ptr<BlockAST> block = nullptr;
+    unique_ptr<LeftValAST> leftVal = nullptr;
+    unique_ptr<LowAST> exp = nullptr;
+
+    void setLeftVal(LeftValAST* leftVal);
+    void setStmtType(STMT_TYPE stmtType);
+    void setExpression(LowAST* exp);
+    void setBlock(BlockAST* block);
+    void setReturn(ReturnStmtAST* returnStmt);
+    void setSelect(SelectStmtAST* select);
+    void setIteration(IterationStmtAST* iteration);
+
+};
+
+class UnaryAST {
+public:
+    unique_ptr<PrimaryExpAST> primaryExp;
+    unique_ptr<CallAST> call;
+    unique_ptr<UnaryAST> unary;
+    U_OP op;
+
+    void setOp(U_OP op);
+
+    void setPrimaryExp(PrimaryExpAST* primaryExp);
+    void setCall(CallAST* call);
+    void setUnary(UnaryAST* unaryAst);
+
+};
+
+class Visitor {
+public:
+    //重写该方法
+    virtual void visit(AST& ast) = 0;
+    //TODO:增加相应的遍历方法
 };
 
 
