6ff3b1ece152da0958eae07f0f9150ffaf2f274f
LazySheeeeep <121164210+LazySheeeeep@users.noreply.github.com>
2022-12-24 15:52:39 +0800

    Add files via upload

---
+++ CS232/homeworks/compiler/src/Pass.h
@@ -0,0 +1,36 @@
+#ifndef PASS_H
+#define PASS_H
+
+
+#include <string>
+#include <list>
+#include <algorithm>
+#include "IR.h"
+
+
+class Pass{
+public:
+    explicit Pass(Module* m){module = m;}
+    virtual void execute() = 0;
+    virtual const std::string get_name() const = 0;
+    Module* module;
+};
+
+template<typename T>
+using PassList = std::list<T*>;
+
+class PassMgr{
+public:
+    explicit PassMgr(Module* m){module = m;pass_list = PassList<Pass>();}
+    template <typename PassTy> void addPass(){pass_list.push_back(new PassTy(module));}
+    void execute() {
+        for (auto pass : pass_list) {
+            pass->execute();
+        }
+    }
+    Module* module;
+    PassList<Pass> pass_list;
+};
+
+
+#endif // PASS_H
